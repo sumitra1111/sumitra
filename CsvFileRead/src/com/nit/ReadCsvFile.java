86f4b39fbae7c17b966d43d64b7b1e11855cb5a6
SUMishra <SUMishra@WID-1109.PABNG.COM>
2017-05-19 15:53:26 +0530

    Push to Git

---
+++ CsvFileRead/src/com/nit/ReadCsvFile.java
@@ -0,0 +1,84 @@
+package com.nit;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ReadCsvFile {
+	  
+
+	public static void main(String[] args) {
+		
+	/* try {
+             
+	            //String content = "Hello Learner !! Welcome to howtodoinjava.com.";
+	  
+	            File file = new File("c://samplefile.txt");
+	            System.out.println("File created in given location");
+	  
+	            FileWriter fw = new FileWriter(file);
+	            BufferedWriter bw = new BufferedWriter(fw);
+	            StringBuilder sb = new StringBuilder();
+	            StringBuilder a=null;
+	            for (int i = 1; i <= 1000; i++) {
+	            	
+				//System.out.println(sb.append("peg").append(i).append("@palnyc.com"));
+	            	 sb.append("peg").append(i).append("@palnyc.com").append("     ").append("password").append('\n');	
+	            	
+	              
+	            // sb.replace(0, sb.length(), "");
+	            
+	            }
+	            bw.write(sb.toString());
+	            bw.close();
+	  
+	        } 
+	        catch (IOException e) 
+	        {
+	            e.printStackTrace();
+	        }
+	    }*/
+		
+	}
+		
+}	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		
+		BufferedReader br = null;
+
+		try {
+
+			String sCurrentLine;
+
+			br = new BufferedReader(new FileReader("C:\\TBC_Testdata.csv"));
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+	}*/
+
+	
